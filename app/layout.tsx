48446e779f9e41bfe2f4d67fe3817fa61e83bf90
Artem Verbat <artem.verbat@gmail.com>
2025-02-20 17:57:54 +0800

    feat: Add logo, title, description, keywords.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from 'react';
-import '@/styles/globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { ThemeSwitcher } from '@/components/theme-switcher';
+import '@/styles/globals.css';
 
 export default function RootLayout({
   children
@@ -10,6 +10,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="OxyBoost" />
+        <meta
+          name="description"
+          content="OxyBoost is the apnea training app for freedivers, athletes, and breathing enthusiasts"
+        />
+        <meta
+          name="keywords"
+          content="apnea, apnea trainer, freedive, sta, static, freediving, breath, breathing, training"
+        />
+        <title>OxyBoost - Apnea Trainer</title>
+      </head>
+
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ThemeSwitcher />
